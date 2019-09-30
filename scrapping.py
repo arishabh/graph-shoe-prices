6baf8f28b316bebeb9a9d3d306683127d11adff3
Rishabh Agrawal <rishabhagrawal41@gmail.com>
2019-09-30 11:15:51 -0400

    Completed scrapping information of shoe, TODO: Putting all this data in a file and graphing

--- scrapping.py
+++ scrapping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from RandomHeaders import *
 
 TOTAL_TRIES = 5
 tries = 0
@@ -7,21 +8,25 @@ tries = 0
 base_url = "https://www.stockx.com"
 main_url = "https://www.stockx.com/sneakers/most-popular"
 
-def scrapping_main():
+def soup(url):
     tries = 0
-    shoes_url=[]
     status = 0
     while((status != 200)  or (tries <= TOTAL_TRIES)): #Try with different headers 5 times
         res = requests.get(base_url, headers=LoadHeader())
-        res = requests.get(main_url, headers=LoadHeader())
-        status = res.status_code()
-        tires += 1
+        res = requests.get(url, headers=LoadHeader())
+        status = res.status_code
+        tries += 1
     
     if(res.status_code != 200):
         print("Couldnt connect to website...")
         return None
     
     s = bs(res.content, "lxml") #Convert into beautiful soup 
+    return s
+
+def scrapping_main():
+    shoes_url=[]
+    s = soup(main_url)
     for a in s.findAll("a", href=True): #All the href values
         if(a['href'] != "/"): #If not empty url
             shoes_url.append(a['href']) #Put them in an array
@@ -33,7 +38,28 @@ def scrapping_main():
         shoes_url[index] = base_url + shoes_url[index]
     return shoes_url
 
-def scrapping_shoe(shoe_url):
-    buy_url = shoe_url[:23] + "buy/" + shoe_url[23:len(shoe_url)]
-    sell_url = shoe_url[:23] + "sell/" + shoe_url[23:len(shoe_url)]
+def scrapping_shoe(shoe_url): #Need base, lowest ask and highest bid prices
+    content = soup(shoe_url)
     
+    shoe_name = content.title.string
+
+    bids = content.findAll("div", {"class": "en-us stat-value stat-small"})
+    lowest_ask = bids[0].text
+    highest_bid = bids[1].text
+    
+    sizes = content.findAll("span", {"class": "bid-ask-sizes"})
+    lowest_ask += " " + (sizes[0].text).split(" ")[1] #Adding the sizes
+    highest_bid += " " + (sizes[1].text).split(" ")[1] 
+    #print(lowest_ask + highest_bid)
+
+    base_price = content.findAll("span", {"class":"sneak-score"})[1].text[1:]
+    spans=content.findAll("span")
+    volatility = None
+    release_data = None
+    for index in range(len(spans)):
+        if (spans[index].text == "Release Date"):
+            release_date = spans[index+1].text
+        if (spans[index].text == "Volatility"):
+            volatility = spans[index+1].text
+
+    return shoe_name, base_price, lowest_ask, highest_bid, release_date, volatility
