8ff84492949c72ad244ac326d76340afbbbc138e
Rishabh Agrawal <rishabhagrawal41@gmail.com>
2019-10-01 16:32:09 -0400

    Reduced the amount of time taken by about half using splitting some code. Code now takes only 4 min instead of 8-10 min

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ shoes_url_path = "info/shoes_url.txt"
 shoes_data_path = "info/shoes_data.txt"
 
 if __name__ == "__main__":
-    main_start = time()
-    new_shoe_urls = scrapping_main()
+    main_start = time()#Start the time
+
+    new_shoe_urls = scrapping_main() #Get all the latest URLS
+
     if(new_shoe_urls == None):
         exit()
+
     old_shoe_urls = read_file(shoes_url_path)
     merged_urls = merge(old_shoe_urls, new_shoe_urls)
-    write_list(shoes_url_path, merged_urls)
+    write_list(shoes_url_path, merged_urls)#Put the merged urls in the file
     
     shoes_data=[]
-    all_shoe_urls = read_file(shoes_url_path)
-    for url in all_shoe_urls:
+    for url in merged_urls:
         start = time()
         data = scrapping_shoe(url)
-        if data != None : shoes_data.append(data) 
+        shoes_data.append(data) if data != None else print("Got None\n") 
         end = time()
-        print("URL " + str(all_shoe_urls.index(url)) + " took " + str(end-start) + "s")
+        print("URL " + str(all_shoe_urls.index(url)+1) + " took " + str(end-start) + "s")
 
     write_list(shoes_data_path, shoes_data)
     main_end = time()
     print("Total time taken: " + str((main_end-main_start)/60) + "min")
-    print("Took about " + str(main_end-main_start/(len(all_shoe_urls)+1)) + "s per scrapping")
+    print("Took about " + str((main_end-main_start)/(len(all_shoe_urls)+1)) + "s per scrapping")
--- scrapping.py
+++ scrapping.py
@@ -10,12 +10,21 @@ main_url = "https://www.stockx.com/sneakers/most-popular"
 
 header = ""
 
+def base_soup():
+    tried = 0
+    status = 0
+    while((status != 200) or (tries <= TOTAL_TRIES)):
+        header = LoadHeader()
+        res = requests.get(base_url, headers=header)
+        status = res.status_code
+        tries += 1
+
+
 def soup(url):
     tries = 0
     status = 0
     while((status != 200)  or (tries <= TOTAL_TRIES)): #Try with different headers 5 times
         header = LoadHeader()
-        res = requests.get(base_url, headers=header)
         res = requests.get(url, headers=header)
         status = res.status_code
         tries += 1
@@ -25,6 +34,7 @@ def soup(url):
         return None
     
     s = bs(res.content, "lxml") #Convert into beautiful soup 
+    print(header)
     return s
 
 def scrapping_main():
@@ -44,12 +54,10 @@ def scrapping_main():
 def scrapping_shoe(shoe_url): #Need base, lowest ask and highest bid prices, valatility and release date
     content = soup(shoe_url)
     
-    shoe_name = content.title.string
+    shoe_name = content.title.string.split(" - ")[0]
 
     bids = content.findAll("div", {"class": "en-us stat-value stat-small"})
-    if (bids == []): 
-        print(header)
-        return None
+    if (bids == []): return None
     lowest_ask = bids[0].text
     highest_bid = bids[1].text
     
@@ -67,7 +75,5 @@ def scrapping_shoe(shoe_url): #Need base, lowest ask and highest bid prices, val
             release_date = spans[index+1].text.strip(" ")
         if (spans[index].text == "Volatility"):
             volatility = spans[index+1].text
-    if ((volatility is None) or (release_date is None)): 
-        print(header)
-        return None 
+    if ((volatility is None) or (release_date is None)): return None 
     return " ".join([shoe_name, base_price, lowest_ask, highest_bid, release_date, volatility])
