d8af733b37b8784907c26f65e29c21cb1e484317
Rishabh Agrawal <rishabhagrawal41@gmail.com>
2019-10-05 20:04:21 -0400

    Making changes with the graph and timing and frequency of data reconrding, later will make it automatic

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from plot import *
 
 shoes_url_path = "info/shoes_url.txt"
 shoes_data_path = "info/shoes_data.txt"
+black_list = []
+
+max_time = 6 # No. of weeks you want the graph to show
+frequency = 2 # Number of time you wantit to check in a day
 
 if __name__ == "__main__":
     main_start = time()#Start the time
@@ -16,6 +20,11 @@ if __name__ == "__main__":
 
     old_shoe_urls = read_file(shoes_url_path)
     merged_urls = merge(old_shoe_urls, new_shoe_urls)
+    for url in black_list:
+        try:
+            merged_urls.remove(url)
+        except:
+            continue
     write_list(shoes_url_path, merged_urls)#Put the merged urls in the file
     
     print("Total URls = " + str(len(merged_urls)))
@@ -23,7 +32,7 @@ if __name__ == "__main__":
     for url in merged_urls:
         start = time()
         data = scrapping_shoe(url)
-        shoes_data.append(data) if data != None else print("\nCouldnt connect to URL below") 
+        shoes_data.append(data) if data != None else black_list.append(url) 
         end = time()
         print("URL " + str(merged_urls.index(url)+1) + " took " + str(end-start) + " s")
     mid_end = time()
@@ -34,6 +43,6 @@ if __name__ == "__main__":
     all_data = old_data + shoes_data
     write_list(shoes_data_path, all_data)
 
-    plot_shoes(merge_shoes_data(all_data))
+    plot_shoes(merge_shoes_data(all_data), max_time, frequency)
     main_end = time()
     print("Total time taken: " + str((main_end-main_start)/60) + " min")
--- plot.py
+++ plot.py
@@ -2,28 +2,42 @@ import matplotlib.pyplot as plt
 
 plot_saves = "info/graphs/"
 
-def plot_shoes(shoes_data):
+def plot_shoes(shoes_data, max_time, freq):
     for data in shoes_data:
-        print(data)
+        #print(data)
         
-        lowest_ask = data[1][1:] if (len(data[1][1:])<20) else data[1][-5:]
-        highest_bid =  data[2][1:] if (len(data[2][1:])<20) else data[2][-5:] 
-        x_axis = list(range(len(lowest_ask)))
-        print(lowest_ask, highest_bid)
+        num_data = 7*freq*max_time
+
+        lowest_ask = data[1][1:] if (len(data[1][1:])<num_data) else data[1][num_data*-1:]
+        highest_bid =  data[2][1:] if (len(data[2][1:])<num_data) else data[2][num_data*-1:] 
+        x_coef = 1/num_data
+        x_axis = []
+        (range(len(lowest_ask))) 
+        #print(lowest_ask, highest_bid)
         
         plt.figure(figsize=(12,10))
         plt.xlabel('Time (weeks)')
         plt.ylabel('Price ($)')
-        plt.xlim(0,10)
+        plt.xlim(0,max_time) #Optional
         
         plt.plot(x_axis, lowest_ask, label='Lowest Ask Price', marker='o')
         plt.plot(x_axis, highest_bid, label='Highest Bid Price', marker='o')
         plt.axhline(data[0][1], color='r', label="Retail Price: $"+str(data[0][1]), linestyle='--')
         plt.title(data[0][0])
         plt.grid(color='grey', linestyle='--')
-        
         plt.legend(loc='best')  # legend text comes from the plot's label parameter.
-        for i in range(len(data[1][1:])):
+              
+        y_bot, y_top = plt.ylim()
+        x_left, x_right = plt.xlim()
+        y_diff = y_top - y_bot
+        x_diff = x_right - x_left
+        
+        x_dist = 2*x_diff/100
+        y_dist = y_diff/100
+        y_pos_text = (y_bot+y_top)/2
+        x_pos_text = x_diff/10
+        
+        for i in range(-5, 0, 1):
             plt.text(x_axis[i]-0.2, (lowest_ask[i]+2), "$"+str(lowest_ask[i]), fontsize=13)
             plt.text(x_axis[i]-0.2, (highest_bid[i]+2), "$"+str(highest_bid[i]), fontsize=13)
         
