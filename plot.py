2a6148d85a12002b0f1b8a08970384bd4d413003
Rishabh Agrawal <rishabhagrawal41@gmail.com>
2019-10-07 11:27:40 -0400

    Almost al changes made witht the graph and it is fully executable now

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 
 plot_saves = "info/graphs/"
 
@@ -12,38 +13,45 @@ def plot_shoes(shoes_data, max_time, freq):
         highest_bid =  data[2][1:] if (len(data[2][1:])<num_data) else data[2][num_data*-1:] 
         x_coef = 1/num_data
         x_axis = []
-        (range(len(lowest_ask))) 
+        for i in range(len(lowest_ask)):
+            x_axis.append(i*x_coef)
+        
         #print(lowest_ask, highest_bid)
         
-        plt.figure(figsize=(12,10))
+        plt.figure(figsize=(15,13))
         plt.xlabel('Time (weeks)')
         plt.ylabel('Price ($)')
-        plt.xlim(0,max_time) #Optional
+        plt.xlim(0,math.ceil(x_axis[-1])) #Optional
         
-        plt.plot(x_axis, lowest_ask, label='Lowest Ask Price', marker='o')
-        plt.plot(x_axis, highest_bid, label='Highest Bid Price', marker='o')
+        plt.plot(x_axis, lowest_ask, label='Lowest Ask Price')
+        plt.plot(x_axis, highest_bid, label='Highest Bid Price')
         plt.axhline(data[0][1], color='r', label="Retail Price: $"+str(data[0][1]), linestyle='--')
         plt.title(data[0][0])
         plt.grid(color='grey', linestyle='--')
         plt.legend(loc='best')  # legend text comes from the plot's label parameter.
               
         y_bot, y_top = plt.ylim()
-        x_left, x_right = plt.xlim()
+        x_min, x_max = plt.xlim()
         y_diff = y_top - y_bot
-        x_diff = x_right - x_left
+        x_diff = x_max - x_min
         
         x_dist = 2*x_diff/100
         y_dist = y_diff/100
         y_pos_text = (y_bot+y_top)/2
-        x_pos_text = x_diff/10
+        x_pos_text = x_max + x_diff/8
         
-        for i in range(-5, 0, 1):
-            plt.text(x_axis[i]-0.2, (lowest_ask[i]+2), "$"+str(lowest_ask[i]), fontsize=13)
-            plt.text(x_axis[i]-0.2, (highest_bid[i]+2), "$"+str(highest_bid[i]), fontsize=13)
-        
+        plt.text(x_axis[-1]-x_dist, (lowest_ask[-1]+y_dist), "$"+str(lowest_ask[-1]), fontsize=13)
+        plt.text(x_axis[-1]-x_dist, (highest_bid[-1]+y_dist), "$"+str(highest_bid[-1]), fontsize=13)
+
+        """
+        start = -5 if len(x_axis)>=5 else len(x_axis)
+        for i in range(start, 0, 1):
+            plt.text(x_axis[i]-x_dist, (lowest_ask[i]+y_dist), "$"+str(lowest_ask[i]), fontsize=13)
+            plt.text(x_axis[i]-x_dist, (highest_bid[i]+y_dist), "$"+str(highest_bid[i]), fontsize=13)
+        """
         text = "Latest data:\n\n" + data[0][2] + "\nHighest Bid Price: $" + str(highest_bid[-1]) + " for Size: " + data[2][0] 
         text += "\nLowest Ask Price: $" + str(lowest_ask[-1]) + " for Size: " + data[1][0] + "\n" + data[0][3] + "\nRetail Price: $" +str(data[0][1])
         
-        plt.text(11, (highest_bid[-1]+lowest_ask[-1])/2, text, fontsize=18)
+        plt.text(x_pos_text, y_pos_text, text, fontsize=18)
         #plt.show()
         plt.savefig(plot_saves+(data[0][0].replace("/", "|"))+".png", bbox_inches="tight")
